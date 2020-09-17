2a0122e028333def9edac815ed9c7af862ea9734
VictorJanampa <52934166+VictorJanampa@users.noreply.github.com>
2020-09-16 20:17:25 -0500

    Update Main.java

--- Java/CountingSort/Main.java
+++ Java/CountingSort/Main.java
@@ -2,13 +2,13 @@ class CountingSort {
   void sort(int arr[]) {
     int n = arr.length;
     int output[] = new int[n];
-    int count[] = new int[n+10];//n+1 Should be the max integer
+    int count[] = new int[n+10];//n must be the max integer
     for (int i = 0; i < n; ++i)
       count[i] = 0;
 
     for (int i = 0; i < n; ++i)
       ++count[arr[i]];
-    for (int i = 1; i <= n; ++i)
+    for (int i = 1; i <= n+10-1; ++i)//n must be the max integer - 1
       count[i] += count[i - 1];
     for (int i = n - 1; i >= 0; i--) {
       output[count[arr[i]] - 1] = arr[i];
