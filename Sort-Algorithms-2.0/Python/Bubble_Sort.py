02b47a54377ff4c1e60eaf85ab3392038718cf07
GinoFernadezMamani <38131971+GinoFernadezMamani@users.noreply.github.com>
2020-09-23 20:42:02 -0500

    Update Bubble_Sort.py

--- Sort-Algorithms-2.0/Python/Bubble_Sort.py
+++ Sort-Algorithms-2.0/Python/Bubble_Sort.py
@@ -1,3 +1,8 @@
+import os
+from time import time
+
+import random
+
 def BubbleSort(V):
     for i in range(0,len(V),1):
         for j in range(0,len(V)-1,1):
@@ -6,6 +11,21 @@ def BubbleSort(V):
                 V[j] = V[j+1]
                 V[j+1] = temp
 
-V = [54,26,93,17,77,31,44,55,20]
-BubbleSort(V)
-print(V)
+def crear_arreglo(A,n):
+    for i in range(n):
+        A.append(random.randint(1,100000))
+        
+
+if __name__=='__main__':
+    A=[100000]
+    f=open("tiempos_bubble_p.txt","w")
+    for i in range(10000,100001,10000):
+        crear_arreglo(A,i)
+        tiempo_inicial=time()
+        BubbleSort(A)
+        tiempo_final=time()
+        tiempo_ejecucion=tiempo_final-tiempo_inicial
+        f.write(str(tiempo_ejecucion))
+        print("tiempo de ejecucion: ",tiempo_ejecucion)
+    f.close()
+        
