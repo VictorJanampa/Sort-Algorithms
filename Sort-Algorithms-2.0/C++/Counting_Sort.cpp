b429c57155092a23e636d5a0edd338279b60dfdd
GinoFernadezMamani <38131971+GinoFernadezMamani@users.noreply.github.com>
2020-09-23 22:32:01 -0500

    Update Counting_Sort.cpp

--- Sort-Algorithms-2.0/C++/Counting_Sort.cpp
+++ Sort-Algorithms-2.0/C++/Counting_Sort.cpp
@@ -1,48 +1,68 @@
-#include <iostream>
+#include <algorithm>
+#include<iostream>
+#include<time.h>
+#include<fstream>
+#include<stdlib.h>
+#include <vector>
 using namespace std;
 
-void countSort(int array[], int size) {
+void countSort(vector<int>& arr,int i)
+{
+    int max = *max_element(arr.begin(), arr.end());
+    int min = *min_element(arr.begin(), arr.end());
+    int range = max - min + 1;
 
-  int output[10];
-  int count[10];
-  int max = array[0];
+    vector<int> count(range), output(arr.size());
+    for (int i = 0; i < arr.size(); i++)
+        count[arr[i] - min]++;
 
-  for (int i = 1; i < size; i++) {
-    if (array[i] > max)
-      max = array[i];
-  }
+    for (int i = 1; i < count.size(); i++)
+        count[i] += count[i - 1];
 
-  for (int i = 0; i <= max; ++i) {
-    count[i] = 0;
-  }
+    for (int i = arr.size() - 1; i >= 0; i--) {
+        output[count[arr[i] - min] - 1] = arr[i];
+        count[arr[i] - min]--;
+    }
 
-  for (int i = 0; i < size; i++) {
-    count[array[i]]++;
-  }
-
-  for (int i = 1; i <= max; i++) {
-    count[i] += count[i - 1];
-  }
-
-  for (int i = size - 1; i >= 0; i--) {
-    output[count[array[i]] - 1] = array[i];
-    count[array[i]]--;
-  }
-
-  for (int i = 0; i < size; i++) {
-    array[i] = output[i];
-  }
+    for (int i = 0; i < arr.size(); i++)
+        arr[i] = output[i];
 }
 
-void printArray(int array[], int size) {
-  for (int i = 0; i < size; i++)
-    cout << array[i] << " ";
-  cout << endl;
+void printArray(vector<int>& arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+    cout << "\n";
 }
 
-int main() {
-  int array[] = {4, 2, 2, 8, 3, 3, 1};
-  int n = sizeof(array) / sizeof(array[0]);
-  countSort(array, n);
-  printArray(array, n);
+
+void crear_arreglo(vector<int>& v,int n){
+    int c;
+    srand(time(NULL));
+
+    for(c=0;c<=n;c++){
+        v[c]=rand()%1000000;
+
+    }
+}
+
+
+int main()
+{
+    unsigned t0,t1;
+    double time;
+    vector<int> v(1000000);
+    ofstream archivo;
+    archivo.open("tiempos_counting_c.txt",ios::out);
+    for(int i=100000;i<=1000000;i+=100000){
+        crear_arreglo(v,i);
+        t0=clock();
+        countSort(v,i-1);
+        t1=clock();
+        time=(double(t1-t0)/CLOCKS_PER_SEC);
+        cout<<"Tiempo de ejecucion: "<<time<<endl;
+        archivo<<time<<endl;
+    }
+    archivo.close();
+    return 0;
 }
